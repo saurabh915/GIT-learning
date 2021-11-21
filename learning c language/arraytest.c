31911f3e032622ea8619c8f369db54c94fa2931c
Saurabh <saurabhspatil4545@gmail.com>
2021-11-21 23:59:05 +0530

    will this work

---
+++ learning c language/arraytest.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+    int  i,number;
+    i=60;
+	
+	int number[5][6]={1,2,4,54,56,59},{2,4,56,656,66,56};
+    printf("enter the number at which you want to store the marks");
+    printf("number 1 is %d,%d",number[1] number[0]);
+      printf("number 2 is %d",number[1]);
+        printf("number 3 is %d",number[2]);
+          printf("number 4 is %d",number[3]);
+            printf("number 5 is %d",number[4]);
+           // scanf("%d",&number[5]);
+            printf("value of 6 number is %d",number[5]);
+    
+    
+	//printf("enter the roll number of student\n");
+	//scanf("%d",&i);// i recived number which is greater than 5 numbers  i.e 66
+	//printf("enter marks of that student");
+//scanf("%d",&number[i]);//number is saved after storage of array is completed i.e. array is for storing 5 numbers 
+//printf("student of roll number %d have marks %d",i,number[i]);
+}
