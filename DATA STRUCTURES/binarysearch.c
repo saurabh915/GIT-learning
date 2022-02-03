24316565b50283fdee4c4bda00c3e2cc734cd0dc
Saurabh <saurabhspatil4545@gmail.com>
2022-02-03 20:56:53 +0530

    started to saving form data to mongodb and started using mongoose

--- DATA STRUCTURES/binarysearch.c
+++ DATA STRUCTURES/binarysearch.c
@@ -1,37 +1,30 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
     int a;
-    int array[50]= {1,12,14,18,66};
+    int array[50] = {1, 12, 14, 18, 66};
     printf("enter the number you want to find out:");
-    scanf("%d",&a);
-    int mid=5/2;
+    scanf("%d", &a);
+    int mid = 5 / 2;
 
-    here:
-   if (array[mid]==a)
-   {
-     printf("your number is found at location %d",mid);
-     
-   }
-   else
-   {
-       if (array[mid]<a)
-       {
-           mid=mid+5/2;
-           goto here;
-           
-       }
-       else
-       {
-           mid=mid/2;
-           goto here;
-       }
-       
-       
-   }
-   
-   
+here:
+    if (array[mid] == a)
+    {
+        printf("your number is found at location %d", mid);
+    }
+    else
+    {
+        if (array[mid] < a)
+        {
+            mid = mid + 5 / 2;
+            goto here;
+        }
+        else
+        {
+            mid = mid / 2;
+            goto here;
+        }
+    }
 
-
-return 0;
+    return 0;
 }
--- DATA STRUCTURES/quicksort.c
+++ DATA STRUCTURES/quicksort.c
@@ -1,5 +1,6 @@
-//this is best amoung all asorting techniques
+//this is best amoung all sorting techniques
 //this use partition technique
+//TIME COMPLEXITY AV.CASE= O(nlogn) best case= O(nlogn)
 
 #include <stdio.h>
 
@@ -14,27 +15,30 @@ void printArray(int *A, int n)      // if we dont want to call array by refferen
 
 int partition(int A[], int low, int high)
 {
-    int pivot = A[low];  // pivot is the first ellement of divided array
+    int pivot = A[low];  // pivot is the first element of divided array
     int i = low + 1;      // i is moved next to pivot point
     int j = high;          // j is moved from high to lowest element  
     int temp;
 
     do
     {
-        while (A[i] <= pivot)
-        {
+        while (A[i] <= pivot /* && i<high*/  )
+        { 
+            printf("%d %d\n",i,A[i]);
             i++;                 //'i' is finding an element which is 'greater' than pivot point hence it is incrementing if the element 
-                                 //  founded element is not greater. other wise as fouded element is greater than pivot it is moving 
+                              //  founded element is not greater. other wise as fouded element is greater than pivot it is moving 
                             // that element to right of the array.
         } 
-
-        while (A[j] > pivot)
+ printf("while quit  'I' %d %d\n",i,A[i]); 
+        while (A[j] > pivot /*&& j>=low*/ )
         {
+             printf("%d %d\n",j,A[j]);
             j--;             //'j' is finding an element which is 'less' than pivot point hence it is decrementing if te element 
                                  //  founded element is not lesser.other wise as fouded element is less than pivot it is moving 
                             // that element to left of the array.
         }
-
+        printf("pivot= %d\n",pivot);
+printf("while quit  'j' %d %d\n",j,A[j]);
         if (i < j)            // the founded elements location indices sould not over crossed thats why if statement is used
         {                     // if they are over crossed then the loop terminates and below swapping is not done and goes for pivot swapping.
             temp = A[i];  
@@ -46,10 +50,11 @@ int partition(int A[], int low, int high)
     // Swap A[low] and A[j]
     temp = A[low];      // why not swapped as pivot point ?---> we dont use pivot to swap as pivot is variable so it
                        // dozent change pivot value at '''array''' pivot value should be changed in array which we are using.
-    A[low] = A[j];        // swapped with j as j is smaller than element founded at i. and smaller elements should be placed
+    A[low] = A[j];        // swapped with j.... as j is smaller than element founded at i. and smaller elements should be placed
                           // at left size of array.  
     A[j] = temp;  
-    return j;          //// j is returned by this function which is used for  high value in another divided array or staring value of divided right array.
+      printArray(A, high);
+    return j;          //// j is returned by this function which is used for  high value in another divided array or starting value of divided right array.
 }
 
 void quickSort(int A[], int low, int high)
@@ -66,12 +71,10 @@ void quickSort(int A[], int low, int high)
 
 int main()
 {
-    //int A[] = {3, 5, 2, 13, 12, 3, 2, 13, 45};
-    int A[] = {9, 4, 4, 8, 7, 5, 6};
-    // 3, 5, 2, 13, 12, 3, 2, 13, 45
-    // 3, 2, 2, 13i, 12, 3j, 5, 13, 45
-    // 3, 2, 2, 3j, 12i, 13, 5, 13, 45 --> first call to partition returns 3
-    int n = 9;
+ 
+    int A[] = {77,11,22,33,44,55,66};
+   
+    int n ;
     n =7;
     printArray(A, n);
     quickSort(A, 0, n - 1);
