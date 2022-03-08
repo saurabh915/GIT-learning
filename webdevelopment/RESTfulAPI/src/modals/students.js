fca5a3e5828152e4d3efa82b5ef3a2507516b0a3
Saurabh <saurabhspatil4545@gmail.com>
2022-03-09 00:24:29 +0530

    created post API

---
+++ webdevelopment/RESTfulAPI/src/modals/students.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const validator  =require("validator");
+const studentSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required: true,
+        minlength: 3
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:[true,"email id already registerd"],
+        validate(value){
+            if (!validator.isEmail(value)) {
+                throw new Error("invalid email")
+            }
+        }
+    },
+    phone:{
+        type:Number,
+        min:10,
+        required:true,// this section is mandetory to fill
+        unique:true//it should be unique
+    },
+    address:{
+        type:String,
+        required:true
+    }
+});
+
+
+
+// we will create new collection
+const Student = new mongoose.model('Student',studentSchema);//here new collection of database is created and the rules are in the new database name (Student )first letter should be capital
+
+module.exports = Student;
